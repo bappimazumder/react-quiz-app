8134643b98d07436d285b640a8bd653acaee5d4e
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2026-01-21 14:51:51 +0600

    main: design Login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,16 +1,27 @@
+import classes from "../../styles/Login.module.css";
+import Button from "../Button";
+import Form from "../Form";
+import Illustration from "../Illustration";
+import TextInput from "../TextInput";
 
+export default function Login() {
+  return (
+    <>
+      <h1>Login to your account</h1>
+      <div class="column">
+        <Illustration />
+        <Form className={`${classes.login}`}>
+          <TextInput
+            type="text"
+            placeholder="Enter email"
+            icon="alternate_email"
+          />
 
-export default function Login(){
-    return(
-        <>
-            <h1>Login to your account</h1>
-            <div class="column">
-                <Illustration />
-                 <Form className={`${classes.login}`}>
-                    
-                </Form>
-            </div>
-        </>
-    );
+          <TextInput type="password" placeholder="Enter password" icon="lock" />
 
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    </>
+  );
 }
