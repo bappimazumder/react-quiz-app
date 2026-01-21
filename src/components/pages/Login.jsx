04e21fef622a194110dfc95847b406cc179f0896
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2026-01-21 14:35:33 +0600

    main: design sign up page

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Login(){
+    return(
+        <>
+            <h1>Login to your account</h1>
+            <div class="column">
+                <Illustration />
+                 <Form className={`${classes.login}`}>
+                    
+                </Form>
+            </div>
+        </>
+    );
+
+}
