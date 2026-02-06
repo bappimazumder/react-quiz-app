be3fb435b579611e24cc945efd082788b23dbbb7
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2026-02-06 18:14:49 +0600

    main: implement result and analysis page with firebase application

--- src/components/pages/Result.jsx
+++ src/components/pages/Result.jsx
@@ -1,12 +1,54 @@
+import _ from "lodash";
+import { useLocation, useParams } from "react-router-dom";
+import useAnswers from "../../hooks/useAnswers";
 import Analysis from "../Analysis";
 import Summary from "../Summary";
-import { useLocation } from "react-router-dom";
 
 export default function Result() {
+  const { id } = useParams();
+  const location = useLocation();
+  const { state } = location;
+  const { qna } = state;
+
+  const { loading, error, answers } = useAnswers(id);
+  console.log(answers);
+
+  function scoreCalculate() {
+    let score = 0;
+    answers.forEach((question, index1) => {
+      let correctIndexex = [];
+      let checkIndexes = [];
+
+      question.options.forEach((option, index2) => {
+        if (option.correct) {
+          correctIndexex.push(index2);
+        }
+        if (qna[index1].options[index2].checked) {
+          checkIndexes.push(index2);
+          option.checked = true;
+        }
+      });
+
+      if (_.isEqual(correctIndexex, checkIndexes)) {
+        score = score + 5;
+      }
+    });
+
+    return score;
+  }
+
+  const userScore = scoreCalculate();
+
   return (
     <>
-      <Summary />
-      <Analysis />
+      {loading && <div> Loading ...</div>}
+      {error && <div>There was an error!</div>}
+      {answers && answers.length > 0 && (
+        <>
+          <Summary score={userScore} noq={answers.length} />
+          <Analysis answers={answers} />
+        </>
+      )}
     </>
   );
 }
