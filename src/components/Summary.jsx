34ca0ab7fd62cc8fb33f668b0df25a1889a5bccb
Bappi Mazumder <bappi.itsd@goldenharvestbd.com>
2026-02-10 17:05:13 +0600

    main: implement api call of pexel

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,46 @@
 import successImage from "../assets/images/success.png";
+import useFetch from "../hooks/useFetch";
 import classes from "../styles/Summary.module.css";
 
 export default function Summary({ score, noq }) {
+  
+  const API_KEY = import.meta.env.VITE_PEXEL_API_KEY;
+
+  const getKeyword = () => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return "failed";
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return "good";
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return "very good";
+    } else {
+      return "excellent";
+    }
+  };
+
+  // Using memo for not multiple loading but its constly
+  // const getKeyword = useMemo(() => {
+  //   if ((score / (noq * 5)) * 100 < 50) {
+  //     return "failed";
+  //   } else if ((score / (noq * 5)) * 100 < 75) {
+  //     return "good";
+  //   } else if ((score / (noq * 5)) * 100 < 100) {
+  //     return "very good";
+  //   } else {
+  //     return "excellent";
+  //   }
+  // }, [score, noq]);
+
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getKeyword()}&per_page=1`,
+    "GET",
+    {
+      Authorization: API_KEY,
+    },
+  );
+
+  const image = result ? result.photos[0].src.medium : successImage;
+
   return (
     <div className={classes.summary}>
       <div className={classes.point}>
@@ -11,9 +50,14 @@ export default function Summary({ score, noq }) {
         </p>
       </div>
 
-      <div className={classes.badge}>
-        <img src={successImage} alt="Success" />
-      </div>
+      {loading && <div className={classes.badge}>Loading your badge...</div>}
+
+      {error && <div className={classes.badge}>An error occured!</div>}
+      {!loading && !error && (
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      )}
     </div>
   );
 }
